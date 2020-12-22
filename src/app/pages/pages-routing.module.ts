530462e0cfe31a6c56d339fed9d28faddb625bcf
johnWNH <jselvamba@gmail.com>
2020-12-22 22:10:36 +0530

    message

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './_layout/layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+      },
+      {
+        path: 'builder',
+        loadChildren: () =>
+          import('./builder/builder.module').then((m) => m.BuilderModule),
+      },
+      {
+        path: 'ecommerce',
+        loadChildren: () =>
+          import('../modules/e-commerce/e-commerce.module').then(
+            (m) => m.ECommerceModule
+          ),
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: '**',
+        redirectTo: 'errors/404',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule { }
